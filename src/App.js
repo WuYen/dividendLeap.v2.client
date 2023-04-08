cc17fa764070c58dc64a8f4bed4c6a7eac61fdf7
Yen <s810643@gmail.com>
2023-04-09 01:26:36 +0800

    Add disply schedule

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [schedule, setSchedule] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await get('https://jolly-bee-garment.cyclic.app/stock/list');
+      console.log('fetch data', data);
+      setSchedule(data.schedule);
+    }
+    fetchData();
+    return () => {};
+  }, [setSchedule]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {schedule.length ? (
+        schedule
+          .sort(({ date: a }, { date: b }) => {
+            if (a < b) {
+              return -1;
+            }
+            if (a > b) {
+              return 1;
+            }
+            return 0;
+          })
+          .map((data, index) => {
+            const formattedDate = `${data.date.slice(0, 4)}-${data.date.slice(4, 6)}-${data.date.slice(6)}`;
+            return (
+              <div key={data.stockNo}>
+                <div style={{ minWidth: '100px', display: 'inline-block' }}>{formattedDate}</div>
+                <div style={{ minWidth: '100px', display: 'inline-block' }}>{data.stockName}</div>
+                <div style={{ minWidth: '50px', display: 'inline-block' }}>{data.stockNo}</div>
+                <div style={{ minWidth: '50px', display: 'inline-block' }}>{data.cashDividen.toFixed(2)}</div>
+              </div>
+            );
+          })
+      ) : (
+        <div>Empty</div>
+      )}
     </div>
   );
+}
+
+function get(url) {
+  return fetch(url, {
+    method: 'GET',
+    // headers: headers(),
+  })
+    .then((res) => res.json())
+    .catch((error) => {
+      console.log('error', error);
+      return {
+        success: false,
+        data: null,
+        error: error.name,
+        message: error.message,
+      };
+    });
 }
 
 export default App;
