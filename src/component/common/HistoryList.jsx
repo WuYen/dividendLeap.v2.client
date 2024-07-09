07e8fb5081cfd147b756019706ecd92ee44a6aff
PEI <s810643@gmail.com>
2024-07-09 11:04:09 +0800

    Remove unused component

--- src/component/common/HistoryList.jsx
+++ src/component/common/HistoryList.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import { toYYYYMMDDWithSeparator } from '../../utility/formatter';
-import { FavoriteButton } from './FavoriteButton';
 import { PostTabs } from './Tabs';
 import StockChart from './StockChart';
 
@@ -85,9 +84,7 @@ export function HistoryList(props) {
                 </div>
               </div>
               {/* row 2 */}
-              <div style={{ gridColumn: '1 / span 3', textAlign: 'left' }}>
-                {toYYYYMMDDWithSeparator(new Date(postInfo.id * 1000))}
-              </div>
+              <div style={{ gridColumn: '1 / span 3', textAlign: 'left' }}>{toYYYYMMDDWithSeparator(new Date(postInfo.id * 1000))}</div>
               <div style={{ textAlign: 'left' }}>
                 <label style={{ fontWeight: 'bold' }}>交易日</label>
                 <div>{baseDateInfo.date ? toYYYYMMDDWithSeparator(baseDateInfo.date) : '-'}</div>
@@ -116,3 +113,32 @@ export function HistoryList(props) {
     </>
   );
 }
+
+const FavoriteButton = (props) => {
+  const [isFavorite, setIsFavorite] = useState(props.isFavorite);
+
+  const handleClick = async () => {
+    setIsFavorite(!isFavorite);
+    const response = await api.get(`/my/post/${props.id}/favorite`);
+    if (!response.success) {
+      setIsFavorite(!isFavorite);
+      alert('收藏失敗');
+    }
+  };
+
+  return (
+    <svg
+      onClick={handleClick}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill={isFavorite ? '#ffe5ae' : 'none'}
+      stroke={isFavorite ? 'gold' : 'gray'}
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      style={{ cursor: 'pointer', width: '24px', height: '24px' }}
+    >
+      <polygon points='12 2 15 8.7 22 9.2 17 14.1 18.5 21 12 17.5 5.5 21 7 14.1 2 9.2 9 8.7 12 2' />
+    </svg>
+  );
+};
