7654bab2023d2bfb345799c4369a0bfd43001042
Yen <s810643@gmail.com>
2024-07-19 20:40:40 +0800

    Fix login wrong verify code will cause error

--- src/component/page/Login.jsx
+++ src/component/page/Login.jsx
@@ -5,20 +5,41 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import api from '../../utility/api';
 import PageTitle from '../common/PageTitle';
 
-export const getLoginStatus = () => {
-  //TODO: move user state to recoil
-  const token = localStorage.getItem('token');
-  if (!!token) {
-    const decoded = jwtDecode(token);
-    const currentTime = Math.floor(Date.now() / 1000);
-    const isValid = decoded.exp - 86400 > currentTime;
-    console.log('exp:', decoded.exp, ', current:', currentTime, ', isValid:', isValid);
-    return [isValid, decoded];
-  } else {
-    return [false, null];
+//TODO: move user state to recoil
+const isValidJWTFormat = (token) => {
+  if (typeof token !== 'string') return false;
+  const parts = token.split('.');
+  if (parts.length !== 3) return false;
+  try {
+    parts.forEach((part) => {
+      if (!/^[A-Za-z0-9-_]+$/.test(part)) throw new Error('Invalid character found');
+      const base64 = part.replace(/-/g, '+').replace(/_/g, '/');
+      atob(base64); // Check if it can be decoded from base64
+    });
+    return true;
+  } catch (e) {
+    return false;
   }
 };
 
+export const getLoginStatus = () => {
+  try {
+    const token = localStorage.getItem('token');
+    if (token && isValidJWTFormat(token)) {
+      const decoded = jwtDecode(token);
+      const currentTime = Math.floor(Date.now() / 1000);
+      const isValid = decoded.exp - 86400 > currentTime;
+      console.log('exp:', decoded.exp, ', current:', currentTime, ', isValid:', isValid);
+      return [isValid, decoded];
+    } else {
+      throw new Error('Invalid token format');
+    }
+  } catch (error) {
+    console.error('Error verifying token:', error);
+    localStorage.removeItem('token');
+    return [false, null];
+  }
+};
 export default function LoginPage(props) {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
